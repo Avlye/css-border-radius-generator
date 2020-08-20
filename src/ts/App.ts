0c16eebb36bd5a3f92a0be8e270fb74abefb16e0
Gabriel Almir <avlye@outlook.com>
2020-08-20 16:25:54 -0300

    Initial Commit

---
+++ src/ts/App.ts
@@ -0,0 +1,58 @@
+export function App() {
+  function handleChangeInput(this: HTMLInputElement) {
+    const inputAction = Input[this.id];
+    const box = document.getElementById('box');
+    const value = this.value || 0;
+
+    if (typeof inputAction === 'function') {
+      inputAction(box, value);
+      codePreview(box);
+    }
+  }
+
+  function codePreview(box: HTMLElement) {
+    const borderRadius = {
+      top_left: box.style.borderTopLeftRadius || 0,
+      top_right: box.style.borderTopRightRadius || 0,
+      bottom_left: box.style.borderBottomLeftRadius || 0,
+      bottom_right: box.style.borderBottomRightRadius || 0
+    }
+
+    const codePreviewText = `
+-webkit-border-radius: ${borderRadius.top_left} ${borderRadius.top_right} ${borderRadius.bottom_left} ${borderRadius.bottom_right};
+-moz-border-radius: ${borderRadius.top_left} ${borderRadius.top_right} ${borderRadius.bottom_left} ${borderRadius.bottom_right};
+border-radius: ${borderRadius.top_left} ${borderRadius.top_right} ${borderRadius.bottom_left} ${borderRadius.bottom_right};`
+
+    const code = document.getElementById('code');
+
+    code.innerHTML = codePreviewText.trim();
+  }
+
+  const Input = {
+    top_left(box: HTMLDivElement, value: string) {
+      console.log("Top Left Changed");
+      box.style.borderTopLeftRadius = `${value}px`;
+    },
+
+    top_right(box: HTMLDivElement, value: string) {
+      console.log("Top Right Changed");
+      box.style.borderTopRightRadius = `${value}px`;
+    },
+
+    bottom_left(box: HTMLDivElement, value: string) {
+      console.log("Bottom Left Changed");
+      box.style.borderBottomLeftRadius = `${value}px`;
+    },
+
+    bottom_right(box: HTMLDivElement, value: string) {
+      console.log("Bottom Right Changed");
+      box.style.borderBottomRightRadius = `${value}px`;
+    }
+  }
+
+  const inputs = document.querySelectorAll(".inputs-container input");
+
+  inputs.forEach(input => {
+    input.addEventListener('change', handleChangeInput);
+  })
+}
